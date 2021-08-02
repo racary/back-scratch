a8a30b67da247c9db8edf9a68e766138596c2d8e
Ryan Cary <rcary@ryans-mbp.myfiosgateway.com>
2021-08-02 13:13:52 -0400

    code deploy

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@ sudo chmod -R 777 /home/ec2-user/back-scratch
 cd /home/ec2-user/express-app
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh"] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_comletion" ] && \. "$NVM_DIR/bash_completion"
 
 npm install
