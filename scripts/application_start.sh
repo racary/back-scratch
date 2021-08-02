5f1d68b64445736189baac2bda1103c3fd446aa6
Ryan Cary <rcary@ryans-mbp.myfiosgateway.com>
2021-08-02 14:16:29 -0400

    fix script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,7 +3,7 @@ cd /home/ec2-user/back-scratch
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_comletion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 npm install
 
